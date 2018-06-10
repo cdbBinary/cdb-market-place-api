79fcf0465f30187cdfce217d2e47f00221c4fe69
charlie.brown <cdb.binary@gmail.com>
2018-06-10 15:26:34 +0100

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 	# Api definition
 	namespace :api, defaults: { format: :json },
 															constraints: { subdomain: 'api' }, path: '/' do
-		# We are going to lis our resources here
+		scope module: :v1 do
+		# We are going to list our resources here
+		end
 	end
 end
+
